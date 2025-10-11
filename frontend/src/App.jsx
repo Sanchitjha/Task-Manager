fb5a00aedd99697b92d0b5aa87c792f5d247259c
Sanchitjha <sanchitjha8888@gmail.com>
2025-10-11 22:10:00 +0530

    Refactor App component to include Navbar and ProtectedRoute for enhanced routing security

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
+import Navbar from './components/Navbar';
+import ProtectedRoute from './components/ProtectedRoute';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Earn from './pages/Earn';
@@ -11,13 +13,32 @@ function App() {
 	return (
 		<AuthProvider>
 			<Router>
+				<Navbar />
 				<Routes>
 					<Route path="/" element={<Home />} />
 					<Route path="/login" element={<Login />} />
-					<Route path="/earn" element={<Earn />} />
-					<Route path="/wallet" element={<Wallet />} />
-					<Route path="/shop" element={<Shop />} />
-					<Route path="/admin" element={<Admin />} />
+					
+					{/* Protected Routes */}
+					<Route path="/earn" element={
+						<ProtectedRoute>
+							<Earn />
+						</ProtectedRoute>
+					} />
+					<Route path="/wallet" element={
+						<ProtectedRoute>
+							<Wallet />
+						</ProtectedRoute>
+					} />
+					<Route path="/shop" element={
+						<ProtectedRoute>
+							<Shop />
+						</ProtectedRoute>
+					} />
+					<Route path="/admin" element={
+						<ProtectedRoute adminOnly>
+							<Admin />
+						</ProtectedRoute>
+					} />
 				</Routes>
 			</Router>
 		</AuthProvider>
