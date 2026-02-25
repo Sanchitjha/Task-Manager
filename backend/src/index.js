38b365411c8fda61d3c08b8c53ece4ad2f31806c
Sanchitjha <sanchitjha8888@gmail.com>
2026-02-25 11:20:31 +0530

    chore: Add localhost:5175 to CORS allowed origins

--- backend/src/index.js
+++ backend/src/index.js
@@ -25,6 +25,7 @@ app.use(cors({
 		'https://task-manager-frontend.vercel.app',
 		'https://www.showcaseretail.in', 
 		'https://showcaseretail.in',
+		'http://localhost:5175',
 		'http://localhost:5174',
 		'http://localhost:5173',
 		'http://localhost:3000'
